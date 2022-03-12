ded9c87dfb38bc3d3a4488d155f1275afea90f41
Hengki Dwiyan Hermawan <hengkidh@gmail.com>
2022-03-12 21:03:08 +0800

    adding slider

--- README.md
+++ README.md
@@ -11,7 +11,8 @@ Password Generator Sederhana
 
 **Versi**
 
-* V.1.0.0 - Rilis awal
+* V 1.0.0 - Rilis awal
+* V 1.0.1 - Menambah slider untuk mempermudah akses di mobile
 
 demo
 https://share.streamlit.io/hengkidh/simplepasswordgenerator/pwgen.py
--- pwgen.py
+++ pwgen.py
@@ -6,10 +6,13 @@ import random
 import string
 import secrets
 
-st.title("Password Generator Sederhana")
-st.caption("Versi 1.0.0")
+st.title("#TooSimplePasswordGenerator")
+st.caption("Versi 1.0.1")
 
-panjang = st.number_input("Masukkan Panjang Karakter", 10)
+st.text("Masukkan panjang karakter menggunakan slider atau input box di bawah ini:")
+nomor = st.slider("Panjang Karakter", 1, 50)
+# st.write("atau")
+panjang = st.number_input("Panjang Karakter", nomor)
 
 if st.button("Generate"):
     rahasia = ''.join(secrets.choice(string.ascii_letters + string.digits) for x in range(panjang))
